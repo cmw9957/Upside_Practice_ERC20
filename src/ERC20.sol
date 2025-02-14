55a03116c4f9c70750644de2b718746248cc1096
cmw9957 <cmw9957@gmail.com>
2025-02-14 09:43:39 +0900

    feat : transfer

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -2,24 +2,35 @@ contract ERC20 {
     string name;
     string symbol;
 
+    address owner;
+
+    bool isPause = false;
+
+    uint256 totalSupply;
+
     constructor (string memory _name, string memory _symbol) {
         name = _name;
         symbol = _symbol;
+
+        owner = msg.sender;
+        totalSupply = 10000 ether;
     }
 
-    function transfer(address _to, uint256 value) public payable {
-
+    function transfer(address _to, uint256 _amount) public payable {
+        require(isPause, "Paused now.");
+        bool success = payable(_to).send(_amount);
+        require(success, "ETH transfer failed.");
     }
 
     function pause() public {
+        
+    }
+
+    function approve(address _spender, uint256 _amount) public payable {
 
     }
 
-    function approve(address _to, uint256 value) public payable {
-
-    }
-
-    function transferFrom(address _from, address _to, uint256 value) public payable {
+    function transferFrom(address _from, address _to, uint256 _amount) public payable {
 
     }
 }
