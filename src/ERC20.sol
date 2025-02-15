0917b3b7ba28d97d3daac722f09a9951932ac57a
cmw9957 <cmw9957@gmail.com>
2025-02-16 00:30:14 +0900

    fix : _toTypedDataHash implementation

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -3,8 +3,18 @@
 pragma solidity ^0.8.0;
 
 contract ERC20 {
-    string name;
-    string symbol;
+    bytes32 private constant TYPE_HASH = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+
+    bytes32 private immutable cachedDomainSeparator;
+    uint256 private immutable cachedChainId;
+    address private immutable cachedThis;
+
+    bytes32 private immutable hashedName;
+    bytes32 private immutable hashedVersion;
+
+    string private immutable name;
+    string private immutable version;
+    string private immutable symbol;
 
     address owner;
     uint256 public constant totalSupply = 1_000_000 * decimal();
@@ -16,8 +26,16 @@ contract ERC20 {
 
     constructor (string memory _name, string memory _symbol) {
         name = _name;
+        version = "1";
         symbol = _symbol;
         owner = msg.sender;
+
+        hashedName = keccak256(bytes(_name));
+        hashedVersion = keccak256(bytes("1"));
+
+        cachedChainId = block.chainid;
+        cachedDomainSeparator = buildDomainSeparator();
+        cachedThis = address(this);
     }
 
     modifier onlyOwner() {
@@ -63,8 +81,30 @@ contract ERC20 {
         balances[_to] += _amount;
     }
 
+    function _domainSeparator() internal view returns (bytes32) {
+        if (address(this) == cachedThis && block.chainid == cachedChainId) {
+            return cachedDomainSeparator;
+        } else {
+            return buildDomainSeparator();
+        }
+    }
+
+    function buildDomainSeparator() private view returns (bytes32) {
+        return keccak256(abi.encode(TYPE_HASH, hashedName, hashedVersion, block.chainid, address(this)));
+    }
+
+    function toTypedDataHash(bytes32 domainSeparator, bytes32 structHash) internal pure returns (bytes32 digest) {
+        assembly ("memory-safe") {
+            let ptr := mload(0x40)
+            mstore(ptr, hex"19_01")
+            mstore(add(ptr, 0x02), domainSeparator)
+            mstore(add(ptr, 0x22), structHash)
+            digest := keccak256(ptr, 0x42)
+        }
+    }
+
     function _toTypedDataHash(bytes32 structHash) public view returns (bytes32) {
-        return _hashTypedDataV4(structHash);
+        return toTypedDataHash(_domainSeparator(), structHash);
     }
 
     function permit(address _owner, address _spender, uint256 _value, uint256 _deadline, uint8 v, bytes32 r, bytes32 s) public isExpire(_deadline) {
